334665bf9f1b4542bc444e9ec9d44995c28221b7
ssah1 <ssah@sapient.com>
2018-05-05 12:52:37 +0530

    file reader

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-(function () {
-    const fs = require('fs');
+const fs = require('fs'),
+    path = require('path');
 
-    fs.readFile('../package.json', 'utf8', function (err, pack) {
+const packageReader = function () {
+    let location = path.join(process.cwd(), 'package.json');
+    fs.readFile(location, 'utf8', function (err, pack) {
         if (err) {
             return console.log(err);
         }
         let packageObj = JSON.parse(pack);
         console.log(packageObj);
     });
-})();
+}
+
+
+packageReader.sendDetails = function () {
+
+}
+
+module.exports = packageReader;
